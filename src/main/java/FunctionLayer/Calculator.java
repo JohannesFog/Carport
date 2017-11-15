0f108b7f040de282da393934efce57588136c72a
Lene Skov <lsko@itu.dk>
2017-11-15 15:00:15 +0100

    added methods to calculator interface and implemented

--- src/main/java/FunctionLayer/Calculator.java
+++ src/main/java/FunctionLayer/Calculator.java
@@ -11,9 +11,21 @@ package FunctionLayer;
  */
 public interface Calculator {
     
+    //Starting point for calculating bill of materials, this method will call other calculating methods
+    public BillOfMaterials bomCalculator(double length, double width, double height);
+    
+    //Calculates the total price of the carport based on the bill of materials
     public double calculatePrice(BillOfMaterials bom);
     
+    public BillOfMaterials calculateStolper(double length, double width, double height);
     
+    public BillOfMaterials calculateTagplader(double length, double width);
     
+    public BillOfMaterials calculateRemme(double length, double width); 
+    
+    //understern, overstern, vandbrædder
+    public BillOfMaterials calculateStern(double length, double width);
+    
+    public BillOfMaterials calculateSpær(double length, double width);
     
 }
--- src/main/java/FunctionLayer/CalculatorImpl.java
+++ src/main/java/FunctionLayer/CalculatorImpl.java
@@ -15,5 +15,35 @@ public class CalculatorImpl implements Calculator {
     public double calculatePrice(BillOfMaterials bom) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public BillOfMaterials bomCalculator(double length, double width, double height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BillOfMaterials calculateStolper(double length, double width, double height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BillOfMaterials calculateTagplader(double length, double width) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BillOfMaterials calculateRemme(double length, double width) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BillOfMaterials calculateStern(double length, double width) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public BillOfMaterials calculateSpær(double length, double width) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
