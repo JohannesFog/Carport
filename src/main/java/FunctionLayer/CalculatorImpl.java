fa9d412d4cfb6f3393e2591656e9dc944e16d854
GertMadsen <cph-gm50@cphbusiness.dk>
2017-11-15 14:19:12 +0100

    entity in english and calculatorImpl added

---
+++ src/main/java/FunctionLayer/CalculatorImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+
+/**
+ *
+ * @author GertLehmann
+ */
+public class CalculatorImpl implements Calculator {
+    
+    @Override
+    public double calculatePrice(BillOfMaterials bom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
