1c156ebb433882c9e74cce5cdff4edd9fc892c2a
Lene Skov <lsko@itu.dk>
2017-11-23 10:36:02 +0100

    updated createOrder command to the new order db

--- src/main/java/PresentationLayer/CreateOrder.java
+++ src/main/java/PresentationLayer/CreateOrder.java
@@ -26,13 +26,19 @@ public class CreateOrder extends Command {
         double length = (double) session.getAttribute("laengde");
         double width = (double) session.getAttribute("bredde");
         double height = (double) session.getAttribute("hoejde");
+        double roofAngle = 0;
+        if(session.getAttribute("vinkel") != null){
+            roofAngle = (double) session.getAttribute("vinkel");
+        }
+        double shedWidth = (double) session.getAttribute("skurbredde");
+        double shedLength = (double) session.getAttribute("skurlaengde");
         String roof = (String) session.getAttribute("tagtype");
         String shed = (String) session.getAttribute("skur");
         String name = request.getParameter("name");
         
         session.setAttribute("name",name);
         
-        LogicFacade.createOrder(length, width, height, roof, shed, name);
+        LogicFacade.createOrder(length, width, height, roof, roofAngle, shed, shedWidth, shedLength, name);
         
         return "confirmationpage";
     }
