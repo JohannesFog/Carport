d5032cdf0d7eaa112fed5746f25d9b7976955825
Lene Skov <lsko@itu.dk>
2017-11-23 09:53:38 +0100

    updated carport db script

--- carport-db.sql
+++ carport-db.sql
@@ -5,12 +5,15 @@ create table `orders`(
     `width` double not null,
     `height` double not null,
     `roof` enum('fladt','skråt') not null,
+    `roof_angle` double,
     `shed` enum('med','uden') not null,
+    `shed_width` double,
+    `shed_length` double,
     `orderdate` date,
     `u_name` varchar(45) null,
     primary key (`o_id`)
 );
 
-insert into `orders`(`length`,`width`,`height`,`roof`,`shed`,`orderdate`,`u_name`) 
-values (510,330,210,'fladt','uden','2017-11-21','Lars Larsen');
+insert into `orders`(`length`,`width`,`height`,`roof`,`roof_angle`,`shed`,`shed_width`,`shed_length`,`orderdate`,`u_name`) 
+values (510,330,210,'fladt',0,'uden',0,0,'2017-11-21','Lars Larsen');
 
