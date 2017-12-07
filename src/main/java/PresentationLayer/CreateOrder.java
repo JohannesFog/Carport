8c541f7cf44093d04eea53a51e52662a539e9f43
Lene Skov <lsko@itu.dk>
2017-12-07 10:26:18 +0100

    createorder henter nu en ordre ud af session

--- src/main/java/PresentationLayer/CreateOrder.java
+++ src/main/java/PresentationLayer/CreateOrder.java
@@ -20,30 +20,38 @@ import javax.servlet.http.HttpSession;
  */
 public class CreateOrder extends Command {
     
-    Order tempOrder;
+    
     public CreateOrder() {
     }
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DataMapperException {
         HttpSession session = request.getSession();
-        double length = (double) session.getAttribute("laengde");
-        double width = (double) session.getAttribute("bredde");
-        double height = (double) session.getAttribute("hoejde");
-        String roof = (String) session.getAttribute("tagtype");
-        double roofAngle = 0;
-        if(session.getAttribute("vinkel") != null){
-            roofAngle = (double) session.getAttribute("vinkel");
-        }
-        double shedWidth = (double) session.getAttribute("skurbredde");
-        double shedLength = (double) session.getAttribute("skurlaengde");
         
-        String role = (String) session.getAttribute("role");
+        //Order
+//        double length = (double) session.getAttribute("laengde");
+//        double width = (double) session.getAttribute("bredde");
+//        double height = (double) session.getAttribute("hoejde");
+//        //String roof = (String) session.getAttribute("tagtype");
+//        //double roofAngle = 0;
+//        double roofAngle = (double) session.getAttribute("vinkel");
+//        double shedWidth = (double) session.getAttribute("skurbredde");
+//        double shedLength = (double) session.getAttribute("skurlaengde");
         
-        Order tempOrder = null;
+        Order order = (Order) session.getAttribute("order");
+        
+        //I den nuværende order i session er status=draft, derfor ændrer vi status nu
+        order.setStatus("unconfirmed");
+        
+        //Order tempOrder = null;
+        
+        //indsætter ordredatoen
         Date date = new Date();
         SimpleDateFormat sdrf = new SimpleDateFormat("yyyy-MM-dd");
         String dateString = sdrf.format(date);
+        order.setOrderDate(dateString);
+        
+        String role = (String) session.getAttribute("role");
         
         if ( role == null || role.equals("employee") ){
           String name = request.getParameter("name");
@@ -55,13 +63,13 @@ public class CreateOrder extends Command {
           
           LogicFacade.createNewUserWithoutPassword(tlf, email, adresse, name, adresse, tlf, role);
           
-          tempOrder = new Order(length, width, height, roofAngle, shedWidth, shedLength, dateString, tlf);
-          LogicFacade.createOrder(tempOrder); 
+          //tempOrder = new Order(length, width, height, roofAngle, shedWidth, shedLength, dateString, tlf);
+          LogicFacade.createOrder(order); 
           
         }else{
             int tlf = Integer.parseInt(request.getParameter("tlf"));
-            tempOrder = new Order(length, width, height, roofAngle, shedWidth, shedLength, dateString, tlf);
-            LogicFacade.createOrder(tempOrder);   
+            //tempOrder = new Order(length, width, height, roofAngle, shedWidth, shedLength, dateString, tlf);
+            LogicFacade.createOrder(order);   
         }
         return "confirmationpage";
     }
