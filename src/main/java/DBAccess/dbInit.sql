a51add7336e2c8878006143a3e8d581f131a70d8
Lene Skov <lsko@itu.dk>
2017-12-12 10:11:38 +0100

    updated db and db-script

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -1,12 +1,14 @@
 create schema carport;
 
+drop table `user`;
+
 create table `user`(
     `id` integer not null auto_increment,
     `name` varchar(45) not null,
     `address` varchar(45) not null,
     `zipcode` integer not null,
-    `phonenumber` integer not null,
-    `email` varchar(45) not null,
+    `phonenumber` integer not null unique,
+    `email` varchar(45) not null unique,
     `password` varchar(45),
     `role` enum('customer','employee') not null,
     primary key (`id`)
@@ -15,6 +17,7 @@ create table `user`(
 insert into `user`(`name`,`address`,`zipcode`,`phonenumber`,`email`,`password`,`role`) values 
 ('Lars Larsen','Hveen Boulevard 8',2630,38383838,'lars@email.com','1234','employee'),
 ('Jens Hansen','Sivsangervej 19',3080,49758900,'jens@mail.dk','5678','customer');
+
 
 create table `orders`(
 	`id` integer not null auto_increment,
@@ -29,6 +32,32 @@ create table `orders`(
     `status` enum ('draft','unconfirmed','confirmed','paid'),
     primary key (`id`)
 );
+
+-- -----------------------------------------------------
+-- Table `carport`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `carport`.`orders` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `length` DOUBLE NOT NULL,
+  `width` DOUBLE NOT NULL,
+  `height` DOUBLE NOT NULL,
+  `roof_angle` DOUBLE NULL DEFAULT NULL,
+  `shed_width` DOUBLE NULL DEFAULT NULL,
+  `shed_length` DOUBLE NULL DEFAULT NULL,
+  `orderdate` DATE NULL DEFAULT NULL,
+  `phonenumber` INT(11) NOT NULL,
+  `status` ENUM('draft', 'unconfirmed', 'confirmed', 'paid') NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_phone_idx` (`phonenumber` ASC),
+  UNIQUE INDEX `phonenumber_UNIQUE` (`phonenumber` ASC),
+  CONSTRAINT `fk_phone`
+    FOREIGN KEY (`phonenumber`)
+    REFERENCES `carport`.`user` (`phonenumber`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 insert into `orders`(`length`,`width`,`height`,`roof_angle`,`shed_width`,`shed_length`,`orderdate`,`phonenumber`,`status`) values 
 (600,300,210,0,270,150,'2017-11-28',40404040,'unconfirmed');
@@ -72,7 +101,7 @@ insert into `materials`(`name`,`price`) values
 ('5,0 x 40 mm. beslagskruer 250 stk.',199.00),
 ('5,0 x 100 mm. skruer 100 stk.',179.00);
 
-drop table `user`;
+
 
 
 
